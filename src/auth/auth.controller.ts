a0e8f4f28267287d695eee580449a45b1022fd13
tjdrkr2580 <tjdrkr2580@naver.com>
2023-01-31 20:21:24 +0900

    ADD | signup, dependency, inject

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Csrf, Msg } from './interfaces/auth.interface';
+import { AuthDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Req,
+  Get,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authservice: AuthService) {}
+  @Post('signup')
+  signUp(@Body() dto: AuthDto): Promise<Msg> {
+    return this.authservice.signUp(dto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import { Msg, Jwt } from './interfaces/auth.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+    private readonly prisma: PrismaService,
+  ) {}
+  async signUp(dto: AuthDto): Promise<Msg> {
+    const hashed = await bcrypt.hash(dto.password, 12);
+    try {
+      await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hashPassword: hashed,
+        },
+      });
+      return {
+        message: 'ok',
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('This Email is Already Taken');
+        }
+      }
+      throw error;
+    }
+  }
+  async login(dto: AuthDto): Promise<Jwt> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Email or Password Incorrect!');
+    const isValid = await bcrypt.compare(dto.password, user.hashPassword);
+    if (!isValid) throw new ForbiddenException('Email or Password Incorrect!');
+    return this.generateJwt(user.id, user.email);
+  }
+
+  async generateJwt(userId: number, email: string): Promise<Jwt> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '5m',
+      secret,
+    });
+    return {
+      accessToken: token,
+    };
+  }
+}
